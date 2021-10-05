eeb36339a16bfa3d68b1fda259cd6d4a7ebfddcf
Mohammad Rifat Ahmed <mohammadrifat884961bnc@gmail.com>
2021-10-05 15:37:56 +0600

    PHP From Validation

---
+++ PHP From Validation/phpform .php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
+<style>
+.error {color: #FF0000;}
+</style>
+
+<?php
+$nameErr = $emailErr = $genderErr = $dateofbirthErr = $degreeErr = $bloodgroupErr = "";
+
+$name = $email = $gender = $dateofbirth = $bloodgroup = $count = "";
+
+$degree = array();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+  if (empty($_POST["name"])) 
+  {
+    $nameErr = "Name is required";
+  } 
+  else 
+  {
+    $name = test_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z-' .]*$/",$name) || str_word_count($name)<2 )
+    {
+      $nameErr = "Only letters and must have two word";
+      $name="";
+    }
+  }
+
+
+  if (empty($_POST["email"])) 
+  {
+    $emailErr = "Email is required";
+  } 
+  else 
+  {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+    {
+      $emailErr = "Invalid email format";
+      $email="";
+    }
+  }
+    
+  if (empty($_POST["birthday"])) 
+  {
+    $dateofbirthErr = "Date of Birth is required";
+  } 
+  else 
+  {
+    $dateofbirth = test_input($_POST["birthday"]);
+  }
+
+  if (empty($_POST["gender"])) 
+  {
+    $genderErr = "Gender is required";
+  } 
+  else 
+  {
+    $gender = test_input($_POST["gender"]);
+  }
+
+  if (empty($_POST["degree"]) ) 
+  {
+    $degreeErr = "Degree is required";
+  } 
+  else 
+  {
+  	foreach($_POST['degree'] as $selected_degree)
+	{
+		$count++;
+	}
+  	if ($count<2)
+  	{
+  		$degreeErr = "Must mark atleast two option";
+  	}
+  	
+  }
+
+    if (empty($_POST["blood_group"])) 
+  {
+    $bloodgroupErr = "Blood_Group is required";
+  } 
+  else 
+  {
+    $bloodgroup = test_input($_POST["blood_group"]);
+  }
+
+}
+
+function test_input($data) 
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Validation Form </h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+
+  Date of Birth: <input type="date" name="birthday">
+  <span class="error">* <?php echo $dateofbirthErr;?></span>
+  <br><br>
+  
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+
+
+
+  Degree:
+  <input type="checkbox" name="degree[]" <?php if (isset($degree) && $degree=="ssc") echo "checked";?> value="ssc">SSC
+  <input type="checkbox" name="degree[]" <?php if (isset($degree) && $degree=="hsc") echo "checked";?> value="hsc">HSC
+  <input type="checkbox" name="degree[]" <?php if (isset($degree) && $degree=="bsc") echo "checked";?> value="bsc">BSc
+  <input type="checkbox" name="degree[]" <?php if (isset($degree) && $degree=="msc") echo "checked";?> value="msc">MSc
+  <span class="error">* <?php echo $degreeErr;?></span>
+  <br><br>
+
+
+
+  Blood Group:
+  <select name="bloodgroup">
+  <option></option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="a+") echo "checked";?> value="a+">A+</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="a-") echo "checked";?> value="a-">A-</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="b+") echo "checked";?> value="b+">B+</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="b-") echo "checked";?> value="b-">B-</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="o+") echo "checked";?> value="o+">O+</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="o-") echo "checked";?> value="o-">O-</option>
+   <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="ab+") echo "checked";?> value="ab+">AB+</option>
+  <option name="bloodgroup" <?php if (isset($bloodgroup) && $bloodgroup=="ab-") echo "checked";?> value="ab-">AB-</option>
+  </select>
+  <span class="error">* <?php echo $bloodgroupErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+
+echo $email;
+echo "<br>";
+
+echo $dateofbirth;
+echo "<br>";
+
+echo $gender;
+echo "<br>";
+
+
+foreach($_POST['degree'] as $selected_degree)
+	{
+		if($count>1)
+		{
+			echo $selected_degree. " ";
+		}
+	}
+echo "<br>";
+
+echo $bloodgroup;
+?>
+
+</body>
+</html>
