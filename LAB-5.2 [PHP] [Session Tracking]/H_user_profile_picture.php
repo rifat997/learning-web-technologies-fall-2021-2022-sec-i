b2e6a70a0d6c0394f7db9097ddf4d6766181b585
Mohammad Rifat Ahmed <mohammadrifat884961bnc@gmail.com>
2021-10-10 19:39:04 +0600

    [Session Tracking

---
+++ LAB-5.2 [PHP] [Session Tracking]/H_user_profile_picture.php
@@ -0,0 +1,16 @@
+
+<html>
+<body>
+  
+<fieldset style="width: 400px">
+<legend>Profile Picture</legend>
+<form action="Profile_Picture_Upload.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <hr>
+  <input type="submit" value="Submit" name="submit">
+
+  
+</form>
+</fieldset>
+</body>
+</html>
